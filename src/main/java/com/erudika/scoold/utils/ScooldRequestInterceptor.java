f16fd5f14b777f207a5d9a46a3a5063cef62cecd
Daniel Yamin <dyamin@allot.com>
2019-09-08 15:01:22 +0300

    logo

--- src/main/java/com/erudika/scoold/utils/ScooldRequestInterceptor.java
+++ src/main/java/com/erudika/scoold/utils/ScooldRequestInterceptor.java
@@ -168,7 +168,7 @@ public class ScooldRequestInterceptor extends HandlerInterceptorAdapter {
 		// Visual customization
 		modelAndView.addObject("navbarFixedClass", Config.getConfigBoolean("fixed_nav", false) ? "navbar-fixed" : "none");
 		modelAndView.addObject("showBranding", Config.getConfigBoolean("show_branding", true));
-		modelAndView.addObject("logoUrl", Config.getConfigParam("logo_url", IMAGESLINK + "/logo.svg"));
+		modelAndView.addObject("logoUrl", Config.getConfigParam("logo_url", IMAGESLINK + "/algoritime.png"));
 		modelAndView.addObject("logoWidth", Config.getConfigInt("logo_width", 90));
 		modelAndView.addObject("stylesheetUrl", Config.getConfigParam("stylesheet_url", STYLESLINK + "/style.css"));
 		// Auth & Badges
